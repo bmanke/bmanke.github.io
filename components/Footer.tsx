fd906c55a8e713dbd019a5f3a54ce23450d10a85
B Manke <bmanke2@nait.ca>
2024-08-19 10:10:41 +0000

    Aug 19, 2024, 4:10 AM

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 import { socialMedia } from "@/data";
 import MagicButton from "./ui/MagicButton";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -30,12 +31,19 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
+            <Link key={info.id} href={info.link}>
+              <div
+                key={info.id}
+                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              >
+                <img
+                  src={info.img}
+                  alt={info.id.toString()}
+                  width={20}
+                  height={20}
+                />
+              </div>
+            </Link>
           ))}
         </div>
       </div>
