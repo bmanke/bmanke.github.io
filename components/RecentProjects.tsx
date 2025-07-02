5ec6fe72e59ee396e91c8061b12c9ae6d7e40b52
B Manke <bmanke2@nait.ca>
2025-07-02 14:28:46 -0600

    Some minor updates July 2nd 2025

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -12,7 +12,7 @@ const RecentProjects = () => {
                 <span className="text-purple">Recent Projects</span>
             </h1>
             <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-10'>
-                {projects.map(({ id, title, des, img, iconLists, link }) => (
+                {projects.map(({ id, title, des, img, iconLists?, link }) => (
                     <div key={id} className='sm:h-[41rem] sm:w-[570px] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center w-[80vw]'>
                         <PinContainer title={link} href={link}>
                             <div className='relative flex items-center justify-center sm:w-[570px] sm:h-[40vh] w-[80vw] overflow-hidden h[30vh] lg:h-[30vh] mb-10'>
@@ -32,23 +32,6 @@ const RecentProjects = () => {
                                 {des}
                             </p>
                             <div className="flex items-center justify-between mt-7 mb-3">
-                                <div className="flex items-center">
-                                    {iconLists.map((icon, index) => (
-                                        <div
-                                            key={index}
-                                            className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                                            style={{
-                                                transform: `translateX(-${5 * index + 2}px)`,
-                                            }}
-                                        >
-                                            <img
-                                                src={icon}
-                                                alt={icon}
-                                                className="p-2"
-                                            />
-                                        </div>
-                                    ))}
-                                </div>
                                 <div className="flex justify-center items-center">
                                     <a href={link} className="flex lg:text-xl md:text-xs text-sm text-purple">Check Live Site</a>
                                     <FaLocationArrow className="ms-3" color="#CBACF9" />
--- data/index.ts
+++ data/index.ts
@@ -72,36 +72,18 @@ export const gridItems = [
 export const projects = [
     {
         id: 1,
-        title: "3D Solar System Planets to Explore",
-        des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-        img: "/p1.svg",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-        link: "https://github.com/adrianhajdin?tab=repositories",
+        title: "Basic HTML and CSS Showcase",
+        des: "A mock website of a restaurant using HTML, CSS and JavaScript with some basic animations.",
+        img: "/sushiman.jpg",
+        link: "https://bmanke.github.io/sushiman/",
     },
     {
         id: 2,
-        title: "Yoom - Video Conferencing App",
-        des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-        img: "/p2.svg",
-        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-        link: "https://github.com/adrianhajdin/zoom-clone",
-    },
-    {
-        id: 3,
-        title: "AI Image SaaS - Canva Application",
-        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-        img: "/p3.svg",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-        link: "https://github.com/adrianhajdin/ai_saas_app",
-    },
-    {
-        id: 4,
-        title: "Animated Apple Iphone 3D Website",
-        des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-        img: "/p4.svg",
-        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-        link: "https://github.com/adrianhajdin/iphone",
-    },
+        title: "Zentry Clone for HypestTeam",
+        des: "A clone of Zentry's website using React, Vite, TailwindCSS and GSAP for animations.",
+        img: "/hypestzentry.jpg",
+        link: "https://bmanke.github.io/awwwards/",
+    }
 ];
 
 export const testimonials = [
@@ -169,7 +151,7 @@ export const workExperience = [
     },
     {
         id: 3,
-        title: "Freelance Web Developer",
+        title: "Freelance Frontend Web Developer",
         desc: "Led the dev of a website for a client, from initial concept to deployment on the internet.",
         className: "md:col-span-2", // change to md:col-span-2
         thumbnail: "/exp3.svg",
