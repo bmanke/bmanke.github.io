21c5258d0d984f44bddfc6c8489666b3dcccb503
B Manke <bmanke2@nait.ca>
2025-07-14 03:58:12 +0000

    Jul 13, 2025, 9:58 PM

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/FloatingNav";
+import Hero from "./components/Hero";
+import { FloatingNav } from "./components/ui/FloatingNav";
 import { FaHome } from 'react-icons/fa';
-import Grid from "@/components/Grid"
-import RecentProjects from "@/components/RecentProjects";
-import { navItems } from "@/data";
-import Clients from "@/components/Clients";
-import Experience from "@/components/Experience";
-import Approach from "@/components/Approach";
-import Footer from "@/components/Footer";
+import Grid from "./components/Grid"
+import RecentProjects from "./components/RecentProjects";
+import { navItems } from "./data";
+import Clients from "./components/Clients";
+import Experience from "./components/Experience";
+import Approach from "./components/Approach";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
